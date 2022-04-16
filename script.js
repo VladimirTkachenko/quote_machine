a490aeaeb5d62679a4a48c13badfd2df2aa8fcb8
VladimirTkachenko <tkachenko.vladimir.kh@gmail.com>
2022-04-16 14:08:42 +0300

    step 1

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log('asd');
+
+async function getQuote() {
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+    const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
+    try {
+        const res = await fetch(proxyUrl + apiUrl);
+        const data = await res.json();
+        console.log(data);
+    } catch(e) {
+        getQuote();
+        console.error('No quote', e);
+    } finally {
+
+    }
+
+}
+
+getQuote();
